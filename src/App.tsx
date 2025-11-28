1b76666a79f8c7ec109111a47371d10ea62a4f0e
alangm16 <alanyahir63@gmail.com>
2025-11-28 12:54:01 -0600

    Cards y Listas

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import Card, { CardBody } from "./components/Card";
+import List from "./components/List";
+
+function App() {
+  const list = ["Goku", "Tanjiro", "Eren"];
+
+  const handleSelect = (elemento: string) => {
+    console.log("imprimiendo", elemento);
+  };
+  const handleSelect2 = (elemento: string) => {
+    console.log("mostrando", elemento);
+  };
+  return (
+    <Card>
+      <CardBody title="Hola Mundo" text="Este es el texto" />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
+    </Card>
+  );
+}
+
+export default App;
