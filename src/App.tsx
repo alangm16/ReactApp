41c31988ffc58880cc4c552674d0b8f7d9664d98
alangm16 <alanyahir63@gmail.com>
2025-12-03 12:39:15 -0600

    Funcionalidades Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
+import { useState } from "react";
+import Button from "./components/Button/";
 import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 
+/**
+ * truthy
+ * falsy
+ * @returns
+ */
+
 function App() {
-  const list = ["Goku", "Tanjiro", "Eren"];
+  const [data, setData] = useState(["Goku", "Tanjiro", "Chanchito Feliz"]);
+  /* const [isLoading, setIsLoading] = useState(false);
+  const handleClick = () => setIsLoading(!isLoading);
+  const list: string[] = ["Goku", "Tanjiro", "Chanchito Feliz"];
 
   const handleSelect = (elemento: string) => {
     console.log("imprimiendo", elemento);
   };
-  const handleSelect2 = (elemento: string) => {
-    console.log("mostrando", elemento);
-  };
+
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} onSelect={handleSelect} />
-      <List data={list} onSelect={handleSelect2} />
+      {list.length !== 0 ? (
+        <List data={list} onSelect={handleSelect} />
+      ) : (
+        "No hay contenido"
+      )}
+      <Button isLoading={isLoading} onClick={handleClick}>
+        Hola Mundo
+      </Button>
+    </Card>
+  ); */
+  const addMinion = () => setData([...data, "Minion"]);
+  const delMinion = () => setData(data.slice(0, -1));
+  return (
+    <Card>
+      <Button isLoading={true} onClick={addMinion}>
+        Agregar
+      </Button>
+      <Button isLoading={false} onClick={delMinion}>
+        Eliminar
+      </Button>
+      <List data={data} />
     </Card>
   );
 }
